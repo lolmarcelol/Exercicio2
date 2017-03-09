211a15b6511c03a860950d4099466e1784ea73d4
marcelo <>
2017-03-09 19:00:49 -0300

    exercicio 2

---
+++ exercicio/src/exercicio/Livro.java
@@ -0,0 +1,10 @@
+package exercicio;
+
+public class Livro {
+
+    private int id;
+    private String titulo;
+    private int ano;
+    private boolean isEmprestado;
+    
+}
---
+++ exercicio/src/exercicio/Usuario.java
@@ -0,0 +1,7 @@
+package exercicio;
+
+public class Usuario {
+    public int id;
+    public String nome;
+    public String senha;
+}
